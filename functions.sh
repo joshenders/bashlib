9334f4d72c2ba9b2be3361c712f092cd458166ee
Josh Enders <josh.enders@gmail.com>
2012-06-26 10:29:41 -0700

    initial commit

---
+++ functions.sh
@@ -0,0 +1,21 @@
+function exit_with_error() {
+    echo "${0##*/}: $@" >&2
+    exit 1
+}
+
+function check_dependencies() {
+    # This function should only consist of builtins
+    # usage: check_dependencies "list of external programs"
+
+    local dependencies="$@"
+
+    for utility in $dependencies; do
+        if [[ "$(type $utility >/dev/null 2>&1; echo $?)" -ne '0' ]]; then
+             echo ${0##*/}: "Could not find \"$utility\". Please verify that \"$utility\" is installed before running this script" >&2
+             exit 1
+        else
+            # set $utility to full path of $utility
+            eval $utility=$(type -P $utility)
+        fi
+    done
+}
