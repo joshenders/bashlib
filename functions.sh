ddca4a5150d33b01e9059ea0d54ae525c97e6be3
Josh Enders <josh.enders@gmail.com>
2012-11-18 22:56:10 -0800

    adds prompt()

--- functions.sh
+++ functions.sh
@@ -12,12 +12,33 @@ function check_dependencies() {
     local dependencies="$@"
 
     for utility in $dependencies; do
-        if [[ "$(type $utility >/dev/null 2>&1; echo $?)" -ne '0' ]]; then
-             echo "${0##*/}: Could not find \"$utility\" in \$PATH. Please verify that \"$utility\" is installed before running this script" >&2
-             exit 1
-        else
-            # set $utility to full path of $utility
-            eval $utility="$(type -P $utility)"
+        if [[ "$(type -t $utility)" != 'builtin' ]]; then
+            if [[ "$(type $utility >/dev/null 2>&1; echo $?)" -ne '0' ]]; then
+                echo "${0##*/}: Could not find \"$utility\" in \$PATH. Please verify that \"$utility\" is installed before running this script" >&2
+                exit 1
+            else
+                # set $utility to full path of $utility
+                eval $utility="$(type -P $utility)"
+             fi
         fi
     done
 }
+
+function prompt() {
+    local message="$1"
+    local response
+
+    while [[ -z "$response" ]]; do
+        read -p "$message [y/n] " response
+        
+        if [[ "$response" =~ ^[yY]$ ]]; then
+            printf "\n"
+            return 0
+        elif [[ "$response" =~ ^[nN]$ ]]; then
+            return 1
+        else
+            unset response
+        fi
+    done
+}
+
