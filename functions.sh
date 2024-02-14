30ca68df2d290d58396157cb1089802fbbf53c1c
Josh Enders <josh.enders@gmail.com>
2024-02-14 00:44:37 -0800

    adds new functions and fixes shellcheck errors
    - fixes shellcheck errors
    - adds format_duration()
    - adds compare_versions()

--- functions.sh
+++ functions.sh
@@ -1,24 +1,66 @@
+# shellcheck shell=bash
+
+format_duration() {
+    local duration="$1"
+    local string hours minutes seconds
+
+    hours=$(((duration % (3600 * 24)) / 3600))
+    if [[ ${hours} -gt 0 ]]; then
+        string+="${hours}h"
+    fi
+
+    minutes=$(((duration % 3600) / 60))
+    if [[ ${minutes} -gt 0 ]]; then
+        string+="${minutes}m"
+    fi
+
+    seconds=$((duration % 60))
+    if [[ ${seconds} -gt 0 ]]; then
+        string+="${seconds}s"
+    fi
+
+    if [[ -n "${string}" ]]; then
+        echo "${string}"
+    fi
+}
+
+compare_versions() {
+    local version1 version2
+    # Pad versions with leading zeros
+    version1=$(printf "%s" "${1}" | awk -F. '{printf "%04d.%04d.%04d.%04d\n", $1, $2, $3, $4}')
+    version2=$(printf "%s" "${2}" | awk -F. '{printf "%04d.%04d.%04d.%04d\n", $1, $2, $3, $4}')
+
+    if [[ "${version1}" == "${version2}" ]]; then
+        return 0
+    elif [[ "${version1}" < "${version2}" ]]; then
+        return 1
+    else
+        return 2
+    fi
+}
+
 function exit_with_error() {
     # usage: exit_with_error "error message"
 
-    echo "${0##*/}: $@" >&2
+    echo "${0##*/}: ${*}" >&2
     exit 1
 }
 
 function check_dependencies() {
     # This function should only consist of builtins
-    # usage: check_dependencies "list of external programs"
+    # usage: check_dependencies "space separated list of external programs"
 
-    local dependencies="$@"
+    local dependencies
+    IFS=" " read -r -a dependencies <<< "${@}"
 
-    for utility in $dependencies; do
-        if [[ "$(type -t $utility)" != 'builtin' ]]; then
-            if [[ "$(type $utility >/dev/null 2>&1; echo $?)" -ne '0' ]]; then
+    for utility in "${dependencies[@]}"; do
+        if [[ "$(type -t "${utility}")" != 'builtin' ]]; then
+            if [[ "$(type "${utility}" >/dev/null 2>&1; echo ${?})" -ne 0 ]]; then
                 echo "${0##*/}: Could not find \"$utility\" in \$PATH. Please verify that \"$utility\" is installed before running this script" >&2
                 exit 1
             else
                 # set $utility to full path of $utility
-                eval $utility="$(type -P $utility)"
+                eval "${utility}"="$(type -P "${utility}")"
              fi
         fi
     done
@@ -31,8 +73,8 @@ function prompt() {
     local response
 
     while [[ -z "$response" ]]; do
-        read -p "$message [y/n] " response
-        
+        read -rp "$message [y/n] " response
+
         if [[ "$response" =~ ^[yY]$ ]]; then
             printf "\n"
             return 0
@@ -53,12 +95,13 @@ function top_level_parent_pid() {
 
     # Look up the parent of the given PID.
     local pid="${1:-$$}"
-    local stat=($(</proc/${pid}/stat))
+    local stat
+    IFS=" " read -r -a stat <<< "$(</proc/"${pid}"/stat)"
     local ppid="${stat[3]}"
 
     # /sbin/init always has a PID of 1, so if you reach that, the current PID is
     # the top-level parent. Otherwise, keep looking.
-    if [[ "${ppid}" -eq '1' ]] ; then
+    if [[ "${ppid}" -eq 1 ]] ; then
         echo "${pid}"
     else
         top_level_parent_pid "${ppid}"
@@ -69,13 +112,13 @@ function exit_with_usage() {
     # usage: exit_with_usage
 
     # cat << EOF >&2 works if you can shell out
-    echo
+    echo \
 "Usage: ${0##*/} [options] <arg>
 
 Options:
     -a N, --apple=N    Do a thing
     -b,   --banana     Do another thing
-" >&2
+"
     exit 1
 
 }
